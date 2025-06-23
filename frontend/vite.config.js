9f91692828e7bd59bd64b6712ec6fe6cd8c4ed2e
mohd-ahsan27 <ahsanbinrauf27@gmail.com>
2025-06-24 00:57:30 +0500

    Updated changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // only needed if you're deploying to a subfolder
   plugins: [react()],
-})
+});
